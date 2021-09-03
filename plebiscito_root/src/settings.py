557843e7b33924ca696f35d3ba2c521ea359475e
asajaroff <asajaroff@users.noreply.github.com>
2021-09-02 21:14:57 -0300

    Attempting to fix statics

--- plebiscito_root/src/settings.py
+++ plebiscito_root/src/settings.py
@@ -155,5 +155,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT="/app/elplebiscito/static/"
-STATIC_URL = '/static/'
+STATIC_ROOT="/app/elplebiscito/"
+STATIC_URL ="/static/"
