3cf7fe3004e1d9c792757fe5357af253b1154ae0
Alejandro Sajaroff <asajaroff@users.noreply.github.com>
2022-06-11 17:00:42 -0300

    Implementacion de AbstractUser para User

--- plebiscito_root/accounts/models.py
+++ plebiscito_root/accounts/models.py
@@ -1,2 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    registration_ip = models.GenericIPAddressField(protocol="both")
+
+    def getIpAddress(self):
+        return "Hello, my name is {}".format(self.first_name)
